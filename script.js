356540f023ed84f182999f821e0b14ec56e2c9a1
Nick <Nick_II2000@mail.ru>
2022-05-31 21:46:48 +0600

    4-th commit

--- script.js
+++ script.js
@@ -3,4 +3,9 @@
 //const arr = [1, 1, 2, 2, 4, 5, 6, 5];
 const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
 const set = new Set(arr);
+// set.add('Ivan');
+// set.add('Oleg');
+set.add('Ivan')
+    .add('Oleg');
+
 console.log(set);
