a84edbeff8629a0d76e4351bbc819b4f5638fd9a
Nick <Nick_II2000@mail.ru>
2022-05-31 21:44:09 +0600

    3-th commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
-const arr = [1, 1, 2, 2, 4, 5, 6, 5];
+//const arr = [1, 1, 2, 2, 4, 5, 6, 5];
+const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
 const set = new Set(arr);
 console.log(set);
